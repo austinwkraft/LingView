06968e2fcdba73efd45e7afb75964cb8e1f07dc5
austinwkraft <102192722+austinwkraft@users.noreply.github.com>
2023-12-03 19:50:11 -0500

    Update AboutPage.jsx

--- jsx/App/AboutPage.jsx
+++ jsx/App/AboutPage.jsx
@@ -5,7 +5,7 @@ import { TranslatableText } from './locale/TranslatableText.jsx'
 const aboutPageJSX = {
   [ENGLISH]:
     <div>
-      <p>This site is a pilot adaptation of LingView by students at McGill University in collaboration with Kanienke'há:ka, for educational purposes.</p>
+      <p>This site is a pilot adaptation of LingView by students at McGill University in collaboration with Kanienke'há:ka, for educational purposes. At this time, the website remains as a pre-release. The material is currently under construction. We thank you for your understanding and interest.</p>
       <p>The purpose of this website is to support Kanien'kéha language learners. The stories available on this website are shared with the consent of the speakers.</p>
     </div>,
   [KANIENKEHA]:
