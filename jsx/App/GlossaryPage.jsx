72d910a1d28a206a2e6cc3d863497c3a3ec94a57
austinwkraft <102192722+austinwkraft@users.noreply.github.com>
2023-12-11 12:43:58 -0500

    Preliminary additions to English GlossaryPage.jsx

--- jsx/App/GlossaryPage.jsx
+++ jsx/App/GlossaryPage.jsx
@@ -5,8 +5,15 @@ import { TranslatableText } from './locale/TranslatableText.jsx'
 const glossaryPageJSX = {
   [ENGLISH] :
     <div>
-      <p>This page could contain short descriptions of relevant glossing abbreviations, and/or a mapping between Leipzig-style glosses and language-internal terminology. </p>
-    </div>,
+      <p><i>Disclaimer; </i>We have done our best to gloss this story as accurately as possible, but it is entirely possible that we have made some mistakes along the way. We welcome and invite anyone to share their insights in places they feel are inaccurate or could use more information. If more stories and glosses are added to this website, we encourage the updating of this glossary page.</p>
+    </div>
+    <div>
+        <ul>
+            <li>CIS - Cislocative: The cislocative indicates direction towards a point of reference with verbs that involve motion in a certain direction.</li>
+            <li>COIN - Coincident: The coincident is used to indicate simultaneous occurrence (i.e. event takes place at the same time as another event) in contexts in which the focus is on the time of the action.</li>
+        </ul>
+    </div>
+  ,
   [KANIENKEHA] :
     <div>
       <p>A Kanien'kéha translation of this page is in progress. </p>
