70221e0a51ddf5a68601fabfd67da891cbe0fe3b
austinwkraft <102192722+austinwkraft@users.noreply.github.com>
2024-04-24 15:55:16 -0400

    Update LandingPage.jsx
    Add link to Kanien'kéha language resources homepage

--- jsx/App/LandingPage.jsx
+++ jsx/App/LandingPage.jsx
@@ -7,7 +7,7 @@ const landingPageJSX = {
     <div>
       <p>Welcome! Click <a href='#/index'>"Index of Texts"</a> to see texts.</p>
       <p>A preliminary version of the site is available in Kanien'kéha. The site language controls are in the footer at the bottom of the window.</p>
-      <p>This website is a final project for LING 415/610 at McGill University, Fall 2023. The site is powered by <a href='https://github.com/BrownCLPS/LingView/' target="_blank" rel="noopener noreferrer">Lingview</a>.</p>
+      <p>This website is a final project for LING 415/610 at McGill University, Fall 2023. Other resources created by members of the McGill linguistics department and Kanien'kéha speakers are available at <a href='https://kanienkeha.lingspace.org/' target="_blank" rel="noopener noreferrer">https://kanienkeha.lingspace.org/</a> The site is powered by <a href='https://github.com/BrownCLPS/LingView/' target="_blank" rel="noopener noreferrer">Lingview</a>.</p>
       <p>For viewing media files on this site, we recommend using the Google Chrome web browser.</p>
     </div>,
   [KANIENKEHA]:
