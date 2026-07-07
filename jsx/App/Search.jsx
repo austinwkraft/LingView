3d6548b308fdd7f8f4856e99c53f58b98a3eb8d7
Lindsay Hempstead <lindsay.hempstead@mail.mcgill.ca>
2026-07-07 12:35:16 -0400

    added tweaks to show/hide filter button text, commented Search.jsx, updated dialect filter logic

--- jsx/App/Search.jsx
+++ jsx/App/Search.jsx
@@ -54,7 +54,6 @@ export class Search extends React.Component {
         document.getElementsByName("fields").forEach(function (e) {
             if (e.checked) fields.push(decode(`dependents.${e.id}.value`));
         });
-        //console.log("fields:", fields);
 
         var options = {
             isCaseSensitive: false,
@@ -67,16 +66,18 @@ export class Search extends React.Component {
             useExtendedSearch: true
         };
 
-        let speakers = [];
+        // Filter sentences to search using the selected filter options
+        let speakers = []; // get list of checked speakers
         document.getElementsByName("speakers").forEach(function (e){
             if (e.checked) speakers.push(decode(e.id));
         });
-        let dialects = [];
+        let dialects = []; // get list of checked dialects
         document.getElementsByName("dialects").forEach(function (e){ 
             if (e.checked) dialects.push(decode(e.id));
         })
-        //console.log("speakers:", speakers);
-        let fileredSentences = this.state.searchIndex.sentences.filter(sentence => {
+        // filter sentences based on speakers, dialects, and duration
+        let filteredSentences = this.state.searchIndex.sentences.filter(sentence => {
+            // only filters on speakers if there are any checked, default is to include all sentences
             if (speakers.length != 0) {
                 if (sentence["speakers"].length == 0) return false;
                 for (const speaker of sentence["speakers"]) {
@@ -85,31 +86,35 @@ export class Search extends React.Component {
                     }
                 }
             }
+            // only filters on dialects if there are any checked, default is to include all sentences
             if (dialects.length != 0) {
                 if (sentence["dialects"].length == 0) return false;
+                // iterate through the dialect filter categories and create a list of accepted language codes associated with the checked dialect categories
+                // dialect categories can be updated by editing the dialect_filter.json file
                 var list = new Set();
                 for (const category of dialects) {
+                    // if any category has no language codes then it accepts all sentences with a language code, so we break the loop with an empty list
+                    // as this will be treated as a catch-all filter
                     if (dialect_filter[category].length == 0) {
                         list.clear();
                         break;
                     } else {
-                        list.add(...dialect_filter[category]);
+                        dialect_filter[category].forEach(dialect => list.add(dialect));
                     }
                 }
+                // if the list is non-empty, then we filter out sentences that don't have a matching language code in the list of accepted codes
                 if (list.size != 0) {
                     if (!sentence["dialects"].some(dialect => list.has(dialect))) {
                         return false;
                     }
                 }
             }
-          //console.log("sentence duration:", sentence["story duration"], "slider value:", document.getElementById("duration").value);
+            // filter out sentences that have a duration greater than the slider value
             if (sentence["story duration"] > document.getElementById("duration").value) return false;
             return true;
         });
-            
-        //console.log("searchIndex.sentences:", this.state.searchIndex.sentences);
-        //console.log("filteredSentences:", fileredSentences);
-        return new Fuse(fileredSentences, options)
+        // searches only on sentences that pass the user filters
+        return new Fuse(filteredSentences, options)
     }
 
     search(rebuild=true) {
@@ -138,45 +143,46 @@ export class Search extends React.Component {
         this.runSearch(false);
     }
 
-    genCheckboxes () { // called by render()
-        let checkboxes = [];
+    genFilters () { // called by render()
+        // generates the tier filter checkboxes, speaker filter checkboxes, dialect filter checkboxes, and duration filter slider
+        let filterOpts = [];
         let tiers = this.state.searchIndex['tier IDs'];
-        checkboxes.push(<label><b>Tiers to search:</b></label>);
+        filterOpts.push(<label><b>Tiers to search:</b></label>);
         tiers.forEach((tier) => {
-            checkboxes.push(
+            filterOpts.push(
                 <input id={htmlEscape(tier)} name="fields" type="checkbox" onChange={this.search.bind(this)}
                 defaultChecked />
             );
-            checkboxes.push(<label>{tier}</label>);
-            checkboxes.push(<span>&nbsp;&nbsp;</span>);
+            filterOpts.push(<label>{tier}</label>);
+            filterOpts.push(<span>&nbsp;&nbsp;</span>);
         })
         let speakerNames = this.state.searchIndex['speaker names'];
-        checkboxes.push(<br />);
-        checkboxes.push(<br />);
-        checkboxes.push(<label><b>Speakers to search:</b></label>);
+        filterOpts.push(<br />);
+        filterOpts.push(<br />);
+        filterOpts.push(<label><b>Speakers to search:</b></label>);
         speakerNames.forEach((speaker) => {
-            checkboxes.push(
+            filterOpts.push(
                 <input id={htmlEscape(speaker)} name="speakers" type="checkbox" onChange={this.search.bind(this)}
                 defaultUnchecked />
             );
-            checkboxes.push(<label>{speaker}</label>);
-            checkboxes.push(<span>&nbsp;&nbsp;</span>);
+            filterOpts.push(<label>{speaker}</label>);
+            filterOpts.push(<span>&nbsp;&nbsp;</span>);
         });
         let dialectNames = Object.keys(dialect_filter);
-        checkboxes.push(<br />);
-        checkboxes.push(<br />);
-        checkboxes.push(<label><b>Dialects to search:</b></label>);
+        filterOpts.push(<br />);
+        filterOpts.push(<br />);
+        filterOpts.push(<label><b>Dialects to search:</b></label>);
         dialectNames.forEach((dialect) => {
-            checkboxes.push(
+            filterOpts.push(
                 <input id={htmlEscape(dialect)} name="dialects" type="checkbox" onChange={this.search.bind(this)}
                 defaultUnchecked />
             );
-            checkboxes.push(<label>{dialect}</label>);
-            checkboxes.push(<span>&nbsp;&nbsp;</span>);
+            filterOpts.push(<label>{dialect}</label>);
+            filterOpts.push(<span>&nbsp;&nbsp;</span>);
         });
-        checkboxes.push(<br />);
-        checkboxes.push(<br />);
-        checkboxes.push(<div class="slidecontainer">
+        filterOpts.push(<br />);
+        filterOpts.push(<br />);
+        filterOpts.push(<div class="slidecontainer">
                         <label><b>Maximum Media Duration:</b></label> <br />
                         <form>
                         <input type="range" class="slider" id="duration" defaultValue={this.state.searchIndex['max duration']} 
@@ -191,16 +197,19 @@ export class Search extends React.Component {
         let button = <button class="searchOptsCollapsible" onClick={this.filterOptionsToggle.bind(this)}>Show Filters</button>;
         
         return <div> {button} <div id="checkboxContainer">
-            {checkboxes}
+             {filterOpts}
         </div></div>;
     }
 
     filterOptionsToggle() {
         let container = document.getElementById("checkboxContainer");
+        let button = document.getElementsByClassName("searchOptsCollapsible")[0];
         if (container.style.display === "none") {
             container.style.display = "block";
+            button.innerHTML = "Hide Filters";
         } else {
             container.style.display = "none";
+            button.innerHTML = "Show Filters";
         }
     }
 
@@ -224,7 +233,7 @@ export class Search extends React.Component {
                 <label for="searchInput"><TranslatableText dictionary={searchPagePromptText} /></label> <input id="searchInput" onChange={this.handleInputChange.bind(this)} type="text" />
                 <p>To search for forms containing two specific elements, separate the elements with a space. For example, 'FACT PUNC' will return all utterances containing a word with both FACT and PUNC. To search for forms containing one element or another, separate the elements with the pipe symbol |. For example, 'FACT|HAB' will return all utterances containing either FACT or HAB.</p>
                 <br />
-                {this.genCheckboxes()}
+                {this.genFilters()}
                 <br />
                 <div><i>Showing {Math.min(this.state.displayedSearchResultsIndex + 1, this.state.searchResults.length)} to {Math.min(this.state.displayedSearchResultsIndex + SEARCH_PAGE_SIZE, this.state.searchResults.length)} of {this.state.searchResults.length} results</i></div>
                 <br />
